74fa1b375d97941da92886b67f3bfb569f1c1c49
Alexander Trost <alexandertrost@trostmacbook.lan>
2019-10-12 18:49:36 -0400

    Add meetup info to events page

--- src/pages/events.js
+++ src/pages/events.js
@@ -3,14 +3,44 @@ import Layout from 'components/layout';
 import Box from 'components/box';
 import Head from 'components/head';
 import Title from 'components/title';
+import { graphql } from 'gatsby';
 
-const Events = ({ data }) => (
+const Events = ({ data: { meetupGroup } }) => (
   <Layout>
-    <Head pageTitle='Events' />
+    <Head pageTitle="Events" />
     <Box>
-      <Title as="h2" size="large">Events</Title>
+      <Title as="h2" size="large">
+        Events
+      </Title>
+      We are currently {meetupGroup.members} members strong!
+      <a href={meetupGroup.link}>Join us!</a>
     </Box>
   </Layout>
 );
+
+export const homeQuery = graphql`
+  query EventsQuery {
+    meetupGroup {
+      members
+      link
+      next_event {
+        time
+      }
+    }
+    allMeetupEvent {
+      edges {
+        node {
+          name
+          local_time
+          link
+          description
+          duration
+          time
+          yes_rsvp_count
+        }
+      }
+    }
+  }
+`;
 
 export default Events;
