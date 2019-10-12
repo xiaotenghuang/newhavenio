a702196e8b594c909e2bca564147e4e36c81ac88
Tim Hwang <timothy@nash.io>
2019-10-12 15:35:51 -0400

    chore: updafte site config

--- site-config.js
+++ site-config.js
@@ -1,16 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  siteTitle: `Gatsby Universal`,
-  siteTitleShort: `GatsbyU`,
-  siteDescription: `An opinionated starter for Gatsby.`,
-  siteUrl: `https://gu.fabianschultz.com`,
-  themeColor: `#000`,
-  backgroundColor: `#fff`,
+  siteTitle: 'New Haven IO',
+  siteTitleShort: 'nh.io',
+  siteDescription: 'Where tech happens in New Haven',
+  siteUrl: 'https://newhaven.io',
+  themeColor: '#000',
+  backgroundColor: '#fff',
   pathPrefix: null,
   logo: path.resolve(__dirname, 'src/images/icon.png'),
   social: {
-    twitter: `gatsbyjs`,
-    fbAppId: `966242223397117`,
+    twitter: 'newhavenio',
   },
 };
