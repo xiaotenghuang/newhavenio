fd3231ec1ee9284f69264dd73c33aa774531c9e7
Alexander Trost <alexrtrost@gmail.com>
2019-10-17 07:44:47 -0400

    Make all elements border-box box sizing

--- src/global.css.js
+++ src/global.css.js
@@ -27,6 +27,7 @@ export default createGlobalStyle`
     font-size: 100%;
     font: inherit;
     vertical-align: baseline;
+    box-sizing: border-box;
   }
 
   /* HTML5 display-role reset for older browsers */
