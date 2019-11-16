1c43a8ba8254cdd7fd3fcadc87e1867fde63f380
Alexander Trost <alexrtrost@gmail.com>
2019-11-16 15:43:12 -0500

    Give fonts fallbacks for better initial renders

--- src/constants/theme/fonts.js
+++ src/constants/theme/fonts.js
@@ -1,2 +1,4 @@
-export const heading = 'Source Sans Pro';
-export const body = 'Lato';
+export const heading =
+  '"Source Sans Pro", -apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"';
+export const body =
+  'Lato, -apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"';
