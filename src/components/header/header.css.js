c08e883c54e1d33c307a78507cebc14055a60c66
Alexander Trost <alexrtrost@gmail.com>
2019-11-16 15:41:52 -0500

    Rework header with headroom

--- src/components/header/header.css.js
+++ src/components/header/header.css.js
@@ -1,28 +1,39 @@
-import styled, { css } from 'styled-components';
+import reactHeadroom from 'react-headroom';
+import styled from 'styled-components';
 
-export const Container = styled.header`
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-  align-items: flex-end;
-  padding: 1rem 4rem;
-  transition: background-color 1s;
+export const Headroom = styled(reactHeadroom)`
+  background: ${p => p.theme.colors.Oranges[100]};
+  height: 140px;
 
-  ${p =>
-    p.isRoot
-      ? null
-      : css`
-          background-color: ${p => p.theme.colors.Oranges[60]};
-          background-image: ${p => p.theme.colors.Gradients.Orange};
-        `};
+  svg {
+    width: 75px;
+    height: 75px;
+    transition: all 0.3s ease;
+  }
+  .headroom--pinned {
+    background: ${p => p.theme.colors.Oranges[100]};
+    svg {
+      width: 25px;
+      height: 25px;
+    }
+  }
 
-  a {
-    color: ${p => p.theme.colors.Whites[100]};
-    transition: color 0.2s ease;
-    text-decoration: none;
+  .headroom--inner {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    align-items: flex-end;
+    padding: 1rem 4rem;
+    transition: background-color 1s;
 
-    &:hover {
-      color: inherit;
+    a {
+      color: ${p => p.theme.colors.Whites[100]};
+      transition: color 0.2s ease;
+      text-decoration: none;
+
+      &:hover {
+        color: inherit;
+      }
     }
   }
 `;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,83 +1,22 @@
-import React from 'react';
-import { Location } from '@reach/router';
-import { Link } from 'gatsby';
-import Button from 'components/shared/button';
-import Title from 'components/shared/title';
-import Text from 'components/shared/text';
-import Box from 'components/shared/box';
-import SplashShapeTop from 'images/splash-shape-top.svg';
 import Nav from 'components/header/nav';
 import Logo from 'components/shared/logo';
 import { theme } from 'constants/theme';
-
-import { Container } from './header.css';
+import { Link } from 'gatsby';
+import React from 'react';
+import * as Styled from './header.css';
 
 const Header = () => (
-  <Location>
-    {({ location }) => (
-      <>
-        <Box
-          display="flex"
-          flexDirection="column"
-          position="relative"
-          padding="0 4rem 2rem"
-          alignItems="flex-start"
-          style={{
-            overflow: 'hidden',
-            height: 'auto',
-          }}
-        >
-          {location.pathname === '/' && (
-            <SplashShapeTop
-              style={{
-                position: 'absolute',
-                top: '-110px',
-                // left: '-4050px',
-                left: '50%',
-                transform: 'translateX(-60%)',
-                marginLeft: '-50vw',
-                zIndex: -1,
-              }}
-            />
-          )}
-          <Container isRoot={location.pathname === '/'}>
-            <Link to="/">
-              <Logo
-                squareColor={theme.colors.Whites[100]}
-                letterColor="transparent"
-              />
-            </Link>
-            <Nav />
-          </Container>
-          {location.pathname === '/' && (
-            <Box
-              display="flex"
-              flexDirection="column"
-              maxWidth="860px"
-              minHeight="480px"
-              position="relative"
-              padding="0 4rem"
-              alignItems="flex-start"
-            >
-              <Title as="h2" size="large" color="Whites.100">
-                Where tech happens in New Haven.
-              </Title>
-              <Box maxWidth="640px">
-                <Text as="p" color="Whites.100">
-                  NewHaven.io is a tech community lorem ipsum dolor sit amet,
-                  consectetur adipiscing elit.
-                </Text>
-              </Box>
-              <Button.Wrapper>
-                <Button palette="secondary">Join the Slack</Button>
-                <Button palette="tertiary">Join the Meetup</Button>
-              </Button.Wrapper>
-            </Box>
-          )}
-        </Box>
-      </>
-    )}
-  </Location>
+  <Styled.Headroom>
+    <div className="headroom--inner">
+      <Link to="/">
+        <Logo
+          squareColor={theme.colors.Whites[100]}
+          letterColor="transparent"
+        />
+      </Link>
+      <Nav />
+    </div>
+  </Styled.Headroom>
 );
 
 export default Header;
