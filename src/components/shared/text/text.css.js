ed66b501204f8bea4774421f9d413cadafafa778
Alexander Trost <alexrtrost@gmail.com>
2019-11-23 17:49:40 -0500

    Add more shared styles properties to text component

--- src/components/shared/text/text.css.js
+++ src/components/shared/text/text.css.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
-import { color } from 'styled-system';
+import { color, typography, space, layout } from 'styled-system';
 
 export const Span = styled.span`
   ${color};
+  ${space};
+  ${layout};
+  ${typography};
 `;
