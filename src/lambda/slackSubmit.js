a4a7b98693139b0016419069deec2c301f007812
timhwang21 <timhwang21@gmail.com>
2019-11-23 17:08:43 -0500

    refactor: pass more info in cloud function
    Currently the cloud function just gives 200 for everything.

--- src/lambda/slackSubmit.js
+++ src/lambda/slackSubmit.js
@@ -3,25 +3,41 @@ import axios from 'axios';
 
 dotenv.config();
 
-export async function handler(event, context, callback) {
+export async function handler(event, _context, callback) {
   const payload = JSON.parse(event.body);
   const email = encodeURIComponent(payload.params.email.trim());
   const SLACK_TOKEN = process.env.SLACK_TOKEN;
   const SLACK_INVITE_ENDPOINT = 'https://slack.com/api/users.admin.invite';
   const toSlack = `email=${email}&token=${SLACK_TOKEN}&set_active=true`;
+
   await axios
     .get(`${SLACK_INVITE_ENDPOINT}?${toSlack}`)
     .then(response => {
-      console.info(response);
+      if (response.data.error != null) {
+        callback(null, {
+          statusCode: 400,
+          body: JSON.stringify({
+            email,
+            error: response.data.error,
+          }),
+        });
+      }
 
       callback(null, {
         statusCode: 200,
         body: JSON.stringify({
-          message: 'success',
+          email,
+          data: response.data,
         }),
       });
     })
     .catch(error => {
-      console.error(error);
+      callback(null, {
+        statusCode: 500,
+        body: JSON.stringify({
+          email,
+          error: error.message,
+        }),
+      });
     });
 }
