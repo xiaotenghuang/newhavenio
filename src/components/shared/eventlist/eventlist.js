90191533c1c17073ced6ad60dc26f17657c7299c
Cody Barr <cody.barr@gmail.com>
2019-11-24 23:25:20 -0500

    fix: 🐛 account for empty case for upcoming events

--- src/components/shared/eventlist/eventlist.js
+++ src/components/shared/eventlist/eventlist.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 
 import Box from 'components/shared/box';
+import Text from 'components/shared/text';
+
 import FeaturedEventCard from 'components/shared/featuredeventcard';
 import EventCard from 'components/shared/eventcard';
 
@@ -34,24 +36,32 @@ const EventList = () => {
   `);
 
   const [nextEvent, ...otherEvents] = data.allMeetupEvent.edges;
-  const events = otherEvents.map(edge => (
-    <EventCard key={edge.id} event={edge.node} />
-  ));
 
   return (
     <Box display="flex" flexDirection="column" width={1}>
-      <Box width={1}>
-        <FeaturedEventCard event={nextEvent.node} />
-      </Box>
-      <Box
-        display="grid"
-        gridTemplateColumns={{ _: '1 fr', lg: '1fr 1fr' }}
-        gridColumnGap={32}
-        gridRowGap={32}
-        mt={32}
-      >
-        {events}
-      </Box>
+      {nextEvent && (
+        <Box width={1}>
+          <FeaturedEventCard event={nextEvent.node} />
+        </Box>
+      )}
+
+      {otherEvents.length > 0 && (
+        <Box
+          display="grid"
+          gridTemplateColumns={{ _: '1 fr', lg: '1fr 1fr' }}
+          gridColumnGap={32}
+          gridRowGap={32}
+          mt={32}
+        >
+          {otherEvents.map(edge => (
+            <EventCard key={edge.id} event={edge.node} />
+          ))}
+        </Box>
+      )}
+
+      {data.allMeetupEvent.edges.length === 0 && (
+        <Text as="p">Sorry, no upcoming events!</Text>
+      )}
     </Box>
   );
 };
