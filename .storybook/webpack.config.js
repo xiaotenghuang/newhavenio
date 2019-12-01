d5cc42d2b47e5eb19b9063b33e4d8c507bb8e329
timhwang21 <timhwang21@gmail.com>
2019-12-01 07:15:36 -0500

    chore: Fix storybook webpack config + add SVG support

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,15 +1,44 @@
 const path = require('path');
 
-// Export a function. Accept the base config as the only param.
-module.exports = async ({ config, _mode }) => {
-  // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-  // You can change the configuration based on that.
-  // 'PRODUCTION' is used when building the static version of storybook.
+module.exports = ({ config }) => {
+  // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
+  config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/];
+  // use installed babel-loader which is v8.0-beta (which is meant to work with @babel/core@7)
+  config.module.rules[0].use[0].loader = require.resolve('babel-loader');
+  // use @babel/preset-react for JSX and env (instead of staged presets)
+  config.module.rules[0].use[0].options.presets = [
+    require.resolve('@babel/preset-react'),
+    require.resolve('@babel/preset-env'),
+  ];
+  config.module.rules[0].use[0].options.plugins = [
+    // use @babel/plugin-proposal-class-properties for class arrow functions
+    require.resolve('@babel/plugin-proposal-class-properties'),
+    // use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
+    require.resolve('babel-plugin-remove-graphql-queries'),
+  ];
+  // Prefer Gatsby ES6 entrypoint (module) over commonjs (main) entrypoint
+  config.resolve.mainFields = ['browser', 'module', 'main'];
 
   // Support absolute imports
   config.resolve.modules.push(path.resolve(__dirname, '../src'));
 
-  // Return the altered config
+  // Support SVGs
+  config.resolve.extensions.push('.svg');
+  // Strip SVG from existing file loader
+  config.module.rules = config.module.rules.map(data =>
+    /svg\|/.test(String(data.test))
+      ? {
+          ...data,
+          test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/,
+        }
+      : data
+  );
+  // Add svg-react-loader, same loader the Gatsby plugin uses
+  config.module.rules.push({
+    test: /\.svg$/,
+    exclude: /node_modules/,
+    use: [{ loader: require.resolve('svg-react-loader') }],
+  });
+
   return config;
 };
-
