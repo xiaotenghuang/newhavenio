a2fea9e6dbea0388e50c037490f53ca1723b6b2f
timhwang21 <timhwang21@gmail.com>
2020-01-14 15:04:41 -0500

    refactor: small style improvements
    - Add shadows for button active for more feedback. Seems like an inset
      shadow is the most consistent with our current styling.
    - Increase consistency of layout in events and resources page.

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import P from 'prop-types';
-import { graphql } from 'gatsby';
 
 import Layout from 'components/layout';
 import Head from 'components/head';
 import Box from 'components/shared/box';
+import Button from 'components/shared/button';
 import Title from 'components/shared/title';
 import Text from 'components/shared/text';
 import EventList from 'components/shared/eventlist/eventlist';
 
-const Events = ({ data: { meetupGroup } }) => (
+const Events = () => (
   <Layout>
     <Head pageTitle="Events" />
     <Box padding="2rem 4rem">
@@ -18,40 +17,30 @@ const Events = ({ data: { meetupGroup } }) => (
       </Title>
       <Text as="p" fontSize="3">
         NewHaven.io hosts tech-focused and social events in the New Haven area
-        that are open for all to attend. We are currently {meetupGroup.members}{' '}
-        members strong!{' '}
-        <a href={meetupGroup.link} target="_blank" rel="noopener noreferrer">
-          Join our Meetup group to be notified of future events
-        </a>
-        .
+        that are open for all to attend.
       </Text>
-      <EventList />
+      <Button
+        as="a"
+        href="https://www.meetup.com/newhavenio/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Join our Meetup group
+      </Button>
+      <Box my={4}>
+        <Title
+          color="Oranges.100"
+          size="small"
+          weight="800"
+          py="1rem"
+          textTransform="uppercase"
+        >
+          Upcoming events
+        </Title>
+        <EventList />
+      </Box>
     </Box>
   </Layout>
 );
-
-Events.propTypes = {
-  data: P.shape({
-    meetupGroup: P.shape({
-      members: P.number.isRequired,
-      link: P.string.isRequired,
-      next_event: P.shape({
-        time: P.string,
-      }).isRequired,
-    }).isRequired,
-  }),
-};
-
-export const homeQuery = graphql`
-  query EventsQuery {
-    meetupGroup {
-      members
-      link
-      next_event {
-        time
-      }
-    }
-  }
-`;
 
 export default Events;
