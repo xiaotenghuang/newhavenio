088b65f8d3ee98aabb268c4fba932c623178524c
timhwang21 <timhwang21@gmail.com>
2020-01-14 20:18:40 -0500

    refactor(Card): Make drop shadow look a bit better
    Makes it a bit tighter.

--- src/components/shared/card/card.css.js
+++ src/components/shared/card/card.css.js
@@ -4,6 +4,6 @@ import Box from 'components/shared/box';
 
 export const Container = styled(Box)`
   padding: 1.5rem;
-  box-shadow: 0px 3px 7px rgba(0, 0, 0, 0.2);
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
   border-radius: 4px;
 `;
