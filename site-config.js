d97251657ea539d229daa9d3605554eb8b10e9f9
timhwang21 <timhwang21@gmail.com>
2020-01-15 14:47:54 -0500

    feat: Opengraph stuff

--- site-config.js
+++ site-config.js
@@ -5,10 +5,11 @@ module.exports = {
   siteTitleShort: 'nh.io',
   siteDescription: 'Where tech happens in New Haven',
   siteUrl: 'https://newhaven.io',
-  themeColor: '#000',
+  themeColor: '#FC6612',
   backgroundColor: '#fff',
   pathPrefix: null,
   logo: path.resolve(__dirname, 'src/images/icon.png'),
+  imageUrl: path.resolve(__dirname, 'static/social.png'),
   social: {
     twitter: 'newhavenio',
   },
